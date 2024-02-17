d6dfbc59fde56a95da365ab7ee95ff0799e6e90e
manen <49951816+manen@users.noreply.github.com>
2024-02-17 15:30:30 +0000

    `puzzle_common`: title is the beta title now

--- common/build.rs
+++ common/build.rs
@@ -4,17 +4,13 @@ fn main() -> anyhow::Result<()> {
 	let hash = bash::run("git rev-parse HEAD")?;
 	let msg = bash::run("git log -1 --pretty=%B")?;
 
-	let title = "puzzle";
-	let title_dbg = format!("puzzle beta ({hash})");
+	let title = format!("puzzle beta ({hash})");
 
 	let gen = format!(
 		"pub const HASH: &'static str = \"{hash}\";
 pub const MSG: &'static str = \"{msg}\";
 
-#[cfg(not(debug_assertions))]
 pub const TITLE: &'static str = \"{title}\";
-#[cfg(debug_assertions)]
-pub const TITLE: &'static str = \"{title_dbg}\";
 "
 	);
 	fs::write("./src/gen.rs", &gen)?;
